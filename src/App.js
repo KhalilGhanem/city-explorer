416e9544806150f89a790548ec69c824d8b35d31
Khalil Ghanem <khalil_ghanem7@yahoo.com>
2021-06-13 14:52:20 +0300

    edit app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+class App extends React.Component {
+  render (){
+    return(
+      <div>
+        <h1>City Explorer</h1>
+        <form>
+          <input type='text' name='city' placeholder='Enter A City' />
+          <input type='submit' value='Explore' />
+        </form>
+      </div>
+    );
+  }
 }
+
 
 export default App;
